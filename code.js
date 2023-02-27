8a1c1cd3a37ec2a5e0966ebccfc2ca2f771c2b09
simptom569 <pikalovegor0@gmail.com>
2023-02-27 21:34:05 +0300

    master

---
+++ code.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(document).on("click", "#about",function(){
+        $("html, body").animate({
+            scrollTop: $(".warper2").offset().top
+        }, 600);
+    });
+});
+
+$(document).ready(function(){
+    $(document).on("click", "#speakers",function(){
+        $("html, body").animate({
+            scrollTop: $(".warper3").offset().top
+        }, 800);
+    });
+});
+
+$(document).ready(function(){
+    $(document).on("click", "#program",function(){
+        $("html, body").animate({
+            scrollTop: $(".warper5").offset().top
+        }, 1500);
+    });
+});
